3b6fae11c52a8350b7ff85028a9c59e3f203148e
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2025-10-11 19:22:25 -0500

    refactor: Revise time logging

--- crates/replay_script/tstl_src/rules/log_time.ts
+++ crates/replay_script/tstl_src/rules/log_time.ts
@@ -2,19 +2,21 @@
 addReplayLib({
   on_nth_tick: {
     [60 * 15]: () => {
-      const seconds = Math.floor(game.ticks_played / 60)
-      const minutes = Math.floor(seconds / 60)
-      const hours = Math.floor(minutes / 60)
+      const totalSeconds = Math.floor(game.ticks_played / 60)
+      const totalMinutes = Math.floor(totalSeconds / 60)
+      const hours = Math.floor(totalMinutes / 60)
+      const seconds = totalSeconds % 60
+      const minutes = totalMinutes % 60
 
       let shouldLog = false
       if (hours < 2) {
-        shouldLog = minutes % 5 === 0 && seconds == 0
+        shouldLog = minutes % 5 == 0 && seconds == 0 // every 5 mins
       } else if (hours < 4) {
-        shouldLog = seconds == 0
+        shouldLog = seconds == 0 // every minute
       } else if (hours < 5) {
-        shouldLog = seconds % 30 === 0
-      } else if (hours < 6) {
-        shouldLog = seconds % 15 === 0
+        shouldLog = seconds % 30 === 0 // every 30 seconds
+      } else {
+        shouldLog = seconds % 15 === 0 // every 15 seconds
       }
 
       if (shouldLog) {
